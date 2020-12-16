55ffff7b22012a2fed42d366d18e638a25c9deb8
Perfect Makanju <damiperfect@gmail.com>
2020-12-16 14:47:12 +0100

    Feature: Storage.ListDirectory

--- packages/storage/src/types.ts
+++ packages/storage/src/types.ts
@@ -2,3 +2,29 @@ export interface CreateFolderRequest {
   path: string;
   bucket: string;
 }
+
+export interface ListDirectoryRequest {
+  path: string;
+  bucket: string;
+  /**
+   * set recursive to true, if you would like all children of folder entries
+   * to be recursively fetched.
+   */
+  recursive?: boolean;
+}
+
+/**
+ * Represent an item stored in a storages directory
+ */
+export interface DirectoryEntry {
+  name: string;
+  path: string;
+  cid: string;
+  isDir: boolean;
+  size: number;
+  items?: DirectoryEntry[];
+}
+
+export interface ListDirectoryResponse {
+  items: DirectoryEntry[];
+}
