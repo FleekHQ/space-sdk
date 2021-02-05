5835947c454fa2b0a98b21785f840158d95f0d48
Perfect Makanju <damiperfect@gmail.com>
2021-02-05 19:53:43 +0100

    Feature: Set a default gundb relay peer

--- packages/storage/src/metadata/gundbMetadataStore.ts
+++ packages/storage/src/metadata/gundbMetadataStore.ts
@@ -79,7 +79,7 @@ export class GundbMetadataStore implements UserMetadataStore {
         this.gun = gunOrServer;
       }
     } else {
-      this.gun = Gun();
+      this.gun = Gun(['http://35.167.205.98:5555/gun']);
     }
 
     this.bucketsListCache = [];
